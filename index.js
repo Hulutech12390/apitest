c028e197316b10f99b926cd24e572adca572153e
alazar <alazarmesfin81@gmail.com>
2023-10-20 12:12:24 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+const cors = require('cors');
+
+
+require("./db/config");
+const user = require('./db/user');
+
+
+app.use(cors());
+app.use(express.json());
+
+
+
+app.post("/register", async(req, resp) => {
+    let userss = new user(req.body);
+    let result = await userss.save();
+    result = result.toObject();
+    delete result.password
+    resp.send(result)
+});
+
+app.post("/login", async (req, resp) => {
+    console.log(req.body)
+    if(req.body.password && req.body.username){
+        let usersss = await user.findOne(req.body).select("-password");
+        if(usersss){
+            resp.send(usersss);
+        }else{
+            resp.send("no user found")
+        }
+    }else{
+        resp.send("no user found")
+    }
+    
+})
+
+app.listen(5000);
